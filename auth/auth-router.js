00f2fbb3e87bc4686690d0370a9ee6332b98095e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-18 13:41:54 -0600

    working on auth-router, dbconfig

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,18 @@
 const router = require('express').Router()
 
+const Users = require('./auth-model')
 
+router.post('/register', (req, res) => {
+
+})
+
+router.post('./login', (req, res) => {
+
+})
+
+router.get('/users', (req, res) => {
+    
+})
 
 module.exports = router
 
