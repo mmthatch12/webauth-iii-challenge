3f69f980b6c7ec4f21889af6fab5d10ea43095a7
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-18 14:11:52 -0600

    workin on register and login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,44 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const Users = require('./auth-model')
 
 router.post('/register', (req, res) => {
+    let user = req.body
+    const hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash
 
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 })
 
 router.post('./login', (req, res) => {
+    let { username, password } = req.body
 
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                const token = generateToken(user)
+                res.status(200).json({ token })
+            } else {
+                res.status(401).json({ message: 'Credentials not valid'})
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json(error)
+        })
 })
 
 router.get('/users', (req, res) => {
-    
+
 })
 
 module.exports = router
